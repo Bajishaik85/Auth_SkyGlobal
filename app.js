cc598d8f8a2610bdeb6fedc6b76eaede4060f99e
BajiShaik85 <bajishaik170085@gmail.com>
2024-05-28 23:35:33 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const { requireAuth, checkUser } = require("./middleware/authMiddleware");
+
+const app = express();
+
+// middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
+
+// database connection
+const dbURI =
+  "mongodb+srv://gofood:gofood2001@cluster0.736b5l3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get("*", checkUser);
+app.use(authRoutes);
